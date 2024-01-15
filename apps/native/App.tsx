c71810c254f533b72146afcde9fe5f7f777c5774
Junie Rosales Jr <jrosales.jr@dnamicro.com>
2024-01-16 05:35:14 +0800

    Update display

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,30 +1,55 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Chip} from '@dnamobile/chip';
-import {DNAButton} from '@rndna/button';
-import {Collapsible} from '@dnamobile/collapsible';
-import {Badge} from '@dnamobile/badge';
-
 const Native = () => {
   return (
     <View style={[styles.container]}>
       <Text style={styles.header}>Native</Text>
-      <Chip
-        label="Chip"
-        isClosable
-        size="default"
-        onPress={() => console.log('pressed')}
-        onPressClose={() => console.log('closeddd')}
-      />
-      <DNAButton label="Button" color="info" />
-      <Collapsible title="Collapsible Title">
-        <View>
-          <Text>Sample Children: Lorem ipsum dolor sit amet</Text>
-        </View>
-      </Collapsible>
-      <Badge>
-        <DNAButton label="Button" color="info" />
-      </Badge>
+      <View style={styles.gap}>
+        <Chip
+          label="Chip"
+          size="sm"
+          onPress={() => console.log('pressed')}
+          onPressClose={() => console.log('closeddd')}
+        />
+        <Chip
+          label="Chip"
+          isClosable
+          size="sm"
+          onPress={() => console.log('pressed')}
+          onPressClose={() => console.log('closeddd')}
+        />
+        <Chip
+          label="Chip"
+          isClosable
+          size="default"
+          onPress={() => console.log('pressed')}
+          onPressClose={() => console.log('closeddd')}
+        />
+        <Chip
+          label="Chip"
+          isDisabled
+          isClosable
+          size="default"
+          onPress={() => console.log('pressed')}
+          onPressClose={() => console.log('closeddd')}
+        />
+        <Chip
+          label="Chip"
+          isClosable
+          size="md"
+          onPress={() => console.log('pressed')}
+          onPressClose={() => console.log('closeddd')}
+        />
+         <Chip
+          label="Chip"
+          isClosable
+          color="success"
+          size="lg"
+          onPress={() => console.log('pressed')}
+          onPressClose={() => console.log('closeddd')}
+        />
+      </View>
     </View>
   );
 };
@@ -43,5 +68,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
     fontSize: 36,
+  },
+  gap: {
+    gap: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
