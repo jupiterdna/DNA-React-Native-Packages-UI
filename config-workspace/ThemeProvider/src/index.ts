f0c3d67085be49845f5d3fba57dbb6b8f82aae51
Juphet Vitualla <juphetvitualla@dnamicro.com>
2024-01-16 05:35:52 +0800

    added theme provider package

---
+++ config-workspace/ThemeProvider/src/index.ts
@@ -0,0 +1,4 @@
+export {ThemeProvider, } from './Provider';
+export {ThemeContext} from './context'
+export type {ThemeProviderProps, colorType, configType, fontSizeType} from './types'
+export {defaultConfig as themeConfig } from './config/index'
