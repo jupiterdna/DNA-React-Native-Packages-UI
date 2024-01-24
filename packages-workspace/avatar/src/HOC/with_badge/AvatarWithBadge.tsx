ae468ef50bf462054fa8e1f5a5d7dba6c89b4ab1
Junie Rosales Jr <jrosales.jr@dnamicro.com>
2024-01-24 08:26:03 +0800

    Add usage of avatar with badge

--- packages-workspace/avatar/src/HOC/with_badge/AvatarWithBadge.tsx
+++ packages-workspace/avatar/src/HOC/with_badge/AvatarWithBadge.tsx
@@ -3,6 +3,28 @@ import { DNAAvatar } from "../../Avatar"
 import { DNAAvatarProps, DNAAvatarPropsWithBadge } from "../../types";
 import { DNABadge } from "@rndna/badge";
 
+/**
+ * An Avatar With Status control can be used to display a badge of the avatar you've displayed.
+ * 
+ *
+ * ## Usage
+ * ```js
+ * import * as React from 'react';
+ * import { DNAAvatarWithBadge } from '@rndna/avatar';
+ *
+ * const MyComponent = () => (
+ *   <DNAAvatarWithBadge
+ *     size="md"
+ *     badge={{
+ *       value: '99+',
+ *       color: 'danger',
+ *     }}
+ *   />
+ *
+ * export default MyComponent;
+ * ```
+ */
+
 const AvatarWithBadge = (Component: React.FC<DNAAvatarProps>) =>
   (props: DNAAvatarPropsWithBadge) => {
     const {
