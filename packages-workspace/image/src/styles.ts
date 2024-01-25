f7716b3572b9e30a137115092f6519f3368246be
Junie Rosales Jr <jrosales.jr@dnamicro.com>
2024-01-26 03:26:22 +0800

    Add lloading on image

--- packages-workspace/image/src/styles.ts
+++ packages-workspace/image/src/styles.ts
@@ -12,6 +12,14 @@ export const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  loadingIndicator: {
+    position: 'absolute',
+    zIndex: 999,
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
+  }
 });
 
 export const imageSizeCls = {
