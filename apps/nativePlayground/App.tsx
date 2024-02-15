ab4791d058be8fcd6c720d751b828a39412558fb
Juphet Vitualla <juphetvitualla@dnamicro.com>
2024-02-16 02:46:43 +0800

    update nativePlayground

---
+++ apps/nativePlayground/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {useColorScheme} from 'react-native';
+import {
+  ThemeProvider,
+  configType,
+  darkmodeColor,
+  themeConfig,
+} from '@rndna/theme-provider';
+import AnimatedScreen from './src/Screens/AnimatedScreen';
+import Testscreen from './src/Screens/Testscreen';
+
+const Native = () => {
+  const rnConfig: configType =
+    useColorScheme() === 'light'
+      ? {
+          ...themeConfig,
+          fonts: {...themeConfig.fonts, fontFamily: 'Roboto'},
+        }
+      : {
+          ...themeConfig,
+          colors: darkmodeColor,
+          fonts: {...themeConfig.fonts, fontFamily: 'Roboto'},
+        };
+
+  return (
+    <ThemeProvider config={rnConfig}>
+      <>
+        {/* <AnimatedScreen /> */}
+        <AnimatedScreen />
+      </>
+    </ThemeProvider>
+  );
+};
+
+export default Native;
