fbbf9de5fab791f2919dbce182cee52c35975a6c
Juphet Vitualla <juphetvitualla@dnamicro.com>
2024-03-13 02:58:19 +0800

    remove log

--- apps/web-docs/src/utils/index.ts
+++ apps/web-docs/src/utils/index.ts
@@ -1,13 +1,11 @@
 import * as Component from "@site/docs/component_usage";
 const capitalize = (str?: string) => str[0].toUpperCase() + str.slice(1);
 export const componentMapper = (id?: string) => {
-  console.log("id", id);
   if (id) {
     const name = id
       .split("-")
       .map((i) => capitalize(i))
       .join("");
-    console.log("name", name);
     return Component[name] ? Component[name]() : null;
   }
   return null;
