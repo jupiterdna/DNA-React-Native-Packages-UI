064d9373892345bbc9914e9c46eff6e4e2f9237f
Juphet Vitualla <juphetvitualla@dnamicro.com>
2024-03-15 07:50:52 +0800

    update home page

--- apps/web-docs/src/components/HomepageFeatures/index.tsx
+++ apps/web-docs/src/components/HomepageFeatures/index.tsx
@@ -1,49 +1,54 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "User Centric",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Prioritize the end-user experience in every aspect of application
+        development. With a keen focus on user-centric design principles, these
+        libraries empower developers to create intuitive, engaging, and
+        accessible interfaces across platforms.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Customizable",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Emphasizes the flexibility and adaptability of your React Native control
+        libraries, allowing developers to tailor the appearance and behavior of
+        components to meet specific project requirements and design preferences.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Wide Vaiety of Controls",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Extensive array of controls with our React Native library, offering a
+        diverse selection to suit every need. From essential UI elements and
+        advanced components such as charts and maps, and caters to a wide range
+        of application requirements.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
@@ -58,7 +63,8 @@ function Feature({title, Svg, description}: FeatureItem) {
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
-      <div className="container">
+      <div className="container py-8">
+        <h2 className="text-center text-4xl">Why DNA React Native Packages?</h2>
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
--- apps/web-docs/src/pages/index.tsx
+++ apps/web-docs/src/pages/index.tsx
@@ -4,29 +4,60 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
+import img from "@site/static/img/dark-logo.png";
 
 import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/getting-started/introduction"
-          >
-            Get Started
-          </Link>
+    <header className="bg-blue-400 dark:bg-gray-700 text-white">
+      <div className="p-4 py-12">
+        <div className="flex flex-row items-center gap-x-12 max-w-[1400px] justify-center mx-auto">
+          <div className="bg-white w-[320px] h-[320px] p-6 shrink-0 justify-center flex items-center">
+            <img src={img} alt="logo" />
+          </div>
+          <div>
+            <h1 className="text-5xl">Cross Platform UI Controls</h1>
+            <h2 className="text-3xl mb-8">DNA React Native UI Packages</h2>
+            <p className="text-lg leading-12">
+              a comprehensive library designed to provide developers with a
+              versatile set of cross-platform UI controls for building robust
+              applications using React Native framework. Whether you're
+              targeting iOS or Android, this collection offers an array of
+              customizable components to streamline your development process and
+              enhance user experience.
+            </p>
+            <Link
+              className="button button--secondary mt-8 button--lg !text-gray-600"
+              to="/docs/getting-started/introduction"
+            >
+              Get Started
+            </Link>
+          </div>
         </div>
       </div>
     </header>
   );
+  // return (
+  //   <header className={clsx("hero hero--primary", styles.heroBanner)}>
+  //     <div className="container">
+  //       <Heading as="h1" className="hero__title">
+  //         {/* {siteConfig.title} */}
+  //         Cross Platform
+  //       </Heading>
+  //       <p className="hero__subtitle">{siteConfig.tagline}</p>
+  //       <div className={styles.buttons}>
+  //         <Link
+  //           className="button button--secondary button--lg"
+  //           to="/docs/getting-started/introduction"
+  //         >
+  //           Get Started
+  //         </Link>
+  //       </div>
+  //     </div>
+  //   </header>
+  // );
 }
 
 export default function Home(): JSX.Element {
