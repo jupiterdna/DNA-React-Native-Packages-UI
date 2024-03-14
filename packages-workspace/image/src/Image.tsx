ed47c02f95a184b672325d763b4ca8e7a18bb869
Juphet Vitualla <juphetvitualla@dnamicro.com>
2024-03-15 06:27:45 +0800

    update image

--- packages-workspace/image/src/Image.tsx
+++ packages-workspace/image/src/Image.tsx
@@ -36,60 +36,71 @@ export const DNAImage: React.FC<DNAImageProps> = React.forwardRef(
       fit = "cover",
       ...restProps
     }: DNAImageProps,
-    ref: React.Ref<View>,
+    ref: React.Ref<View>
   ) => {
     const [isLoading, setIsLoading] = useState(true);
     const themeColor = useColor();
+    const color = themeColor["primary"][50];
+
     const loadingColor = themeColor["primary"][500];
-    const resizeFit = fit === "contain" ? FastImage.resizeMode.contain : FastImage.resizeMode.cover;
+    const resizeFit =
+      fit === "contain"
+        ? FastImage.resizeMode.contain
+        : FastImage.resizeMode.cover;
+
+    const bgColor = {
+      backgroundColor: color,
+    };
 
     const onLoadEnd = () => {
-        setIsLoading(false);
+      setIsLoading(false);
     };
 
     /**
      * This function '_renderLoading' is a memoized callback that returns a JSX element or null.
      * It's used to render a loading indicator when the image is loading.
-     * 
+     *
      * @returns A JSX element representing the loading indicator, or null if 'isLoading' is false.
      */
     const _renderLoading = useCallback((): React.JSX.Element | null => {
-      return (
-        isLoading ? 
-          <View style={[imageSizeCls[size], styles.imageWrapper, styles.loadingIndicator]}>
-              <ActivityIndicator size="small" color={loadingColor} style={styles.image}/>
-          </View>
-        : null
-      )
-    }, [isLoading, size, loadingColor])
+      return isLoading ? (
+        <View style={[imageSizeCls[size], bgColor, styles.loadingIndicator]}>
+          <ActivityIndicator
+            size="small"
+            color={loadingColor}
+            style={styles.image}
+          />
+        </View>
+      ) : null;
+    }, [isLoading, size, loadingColor]);
 
     /**
      * This function '_renderImage' is a memoized callback that returns a JSX element representing the 'FastImage' component.
      * It's used to render the image with the specified source URI, resize mode, and additional props.
      * The image's source is an object with the 'uri', 'priority', and 'cache' properties.
-     * 
+     *
      * @returns A JSX element representing the 'FastImage' component.
      */
     const _renderImage = useCallback((): React.JSX.Element => {
-       return ( 
-          <FastImage
-            style={[styles.image, borderRadiusCls[borderRadius]]}
-            source={{
-              uri: src,
-              priority: FastImage.priority.high,
-              cache: FastImage.cacheControl.immutable
-            }}
-            resizeMode={resizeFit}
-            onLoadEnd={onLoadEnd}
-            {...restProps}
-          />
-        )
-    }, [borderRadius, src, resizeFit, onLoadEnd])
+      return (
+        <FastImage
+          style={[styles.image, borderRadiusCls[borderRadius]]}
+          source={{
+            uri: src,
+            priority: FastImage.priority.high,
+            cache: FastImage.cacheControl.immutable,
+          }}
+          resizeMode={resizeFit}
+          onLoadEnd={onLoadEnd}
+          {...restProps}
+        />
+      );
+    }, [borderRadius, src, resizeFit, onLoadEnd]);
 
     return (
       <View
         style={[
-          styles.imageWrapper,
+          bgColor,
           imageRatioCls[ratio],
           imageSizeCls[size],
           borderRadiusCls[borderRadius],
--- packages-workspace/image/src/styles.ts
+++ packages-workspace/image/src/styles.ts
@@ -1,59 +1,52 @@
-import { StyleSheet } from 'react-native';
-import { useColor } from '@rndna/theme-provider';
-
-const themeColor = useColor();
-const bgColor =  themeColor["primary"][50];
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  imageWrapper:{
-    backgroundColor: bgColor,
-  },
   image: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   loadingIndicator: {
-    position: 'absolute',
+    position: "absolute",
     zIndex: 999,
     top: 0,
     right: 0,
     left: 0,
     bottom: 0,
-  }
+  },
 });
 
 export const imageSizeCls = {
   xs: {
-    width: 130
+    width: 130,
   },
   sm: {
-    width: 170
+    width: 170,
   },
   md: {
-    width: 210
+    width: 210,
   },
   lg: {
-    width: 250
+    width: 250,
   },
   xl: {
-    width: 290
+    width: 290,
   },
-}
+};
 
 export const imageRatioCls = {
   "1:1": {
     aspectRatio: 1,
   },
   "3:2": {
-    aspectRatio: 3/2,
+    aspectRatio: 3 / 2,
   },
   "4:3": {
-    aspectRatio: 4/3,
+    aspectRatio: 4 / 3,
   },
   "8:5": {
-    aspectRatio: 8/5,
+    aspectRatio: 8 / 5,
   },
   "16:9": {
-    aspectRatio: 16/9,
+    aspectRatio: 16 / 9,
   },
-}
+};
